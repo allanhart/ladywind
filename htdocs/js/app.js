870664793401b3db0e00e7bd10766fb420cfba0a
Allan Hart <allan.w.hart@gmail.com>
2022-07-19 14:06:49 +0800

    Tuned logo animation

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -8,6 +8,9 @@ $(function () {
   tl.from('#big-W', { x: -25, duration: 1.25 }, 0);
   tl.to('#big-W', { opacity: 1, duration: 1.25 }, 0);
 
-  tl.from('#lady-wind-fashion', { y: 10, duration: 1.25 }, 0.75);
-  tl.to('#lady-wind-fashion', { opacity: 1, duration: 1.25 }, 0.75);
+  tl.from('#lady-wind-fashion', { y: 10, duration: 1.25 }, 0.5);
+  tl.to('#lady-wind-fashion', { opacity: 1, duration: 1.25 }, 0.5);
+
+  tl.to('#dot-left', { opacity: 1, duration: 5 }, 0);
+  tl.to('#dot-right', { opacity: 1, duration: 5 }, 0);
 });
